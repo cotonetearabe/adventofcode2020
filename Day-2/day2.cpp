96b23a82585c7eb1079f0a83d6f42f251f7927aa
Sebastião Pereira <l40143@alunos.uevora.pt>
2020-12-02 19:24:16 +0000

    c++

--- Day-2/day2.cpp
+++ Day-2/day2.cpp
@@ -1,30 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-int solve_part_one() {
-  
-  return -1;
+
+int solve_part_one(int min, int max, char target, string str) {
+    int counter = 0;
+
+    for(int i = 0; i < str.size(); i++)
+        counter += (str[i] == target);
+
+    return (counter >= min && counter <= max ? 1 : 0);
 }
 
-int solve_part_two() {
-  
-  return -1;
+
+int solve_part_two(int idx1, int idx2, char target, string str) {
+    return int((str[idx1] == target) ^ str[idx2] == target);
 }
 
 
-int main() {
-  vector<int> nums;
-  vector<int> res;
+int main() {    
+    int min, max;
+    char target;
+    string str;
 
-  int temp;
+    int part_one = 0, part_two = 0;
 
-  while(cin >> temp)
-    nums.push_back(temp);
-  
-  cout << "Part one result: " << solve_part_one() << endl;
-  cout << "Part two result: " << solve_part_two() << endl;
+    while(scanf("%d-%d %c :", &min, &max, &target) != EOF) {
+        cin >> str;
+        part_one += solve_part_one(min, max, target, str);
+        part_two += solve_part_two(min, max, target, str);
+    }
 
-  return 0;
+    cout << "Day 2 - Part one: " << part_one << endl;
+    cout << "Day 2 - Part two: " << part_two << endl;
+
+    return 0;
 }
