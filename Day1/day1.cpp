21adfbb00d85b307e9ac4262bea03fab7e664b6a
Sebastião Pereira <l40143@alunos.uevora.pt>
2020-12-02 18:17:13 +0000

    bad day2 solve

---
+++ Day1/day1.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+#define TARGET 2020
+#define MIN_SIZE_ONE 2
+#define MIN_SIZE_TWO 3
+
+using namespace std;
+
+// 0, 1, 2, 3, 4, 5
+
+int solve_part_one(vector<int> nums) {
+	vector<int> ret(MIN_SIZE_ONE);
+
+	for(int i = 0; i < nums.size() - 1; i++) {
+		ret[0] = nums[i];
+		for(int j = i + 1; j < nums.size(); j++) {
+			if(nums[i] + nums[j] == TARGET) {
+				ret[1] = nums[j];
+				return ret[0] * ret[1];
+	  }
+	}
+  }
+  return -1;
+}
+
+int solve_part_two(vector<int> nums) {
+	vector<int> ret(MIN_SIZE_TWO);
+
+	for(int i = 0; i < nums.size() - 2; i++) {
+		ret[0] = nums[i];
+		for(int j = i + 1; j < nums.size() - 1; j++) {
+			ret[1] = nums[j];
+			for(int k = j + 1; k < nums.size(); k++) {
+				if(nums[i] + nums[j] + nums[k] == TARGET) {
+					ret[2] = nums[k];
+					return ret[0] * ret[1] * ret[2];
+				}
+			}
+		}
+	}
+	return -1;
+}
+
+
+int main() {
+	vector<int> nums;
+	vector<int> res;
+
+	int temp;
+
+	while(cin >> temp)
+		nums.push_back(temp);
+
+	cout << "Part one result: " << solve_part_one(nums) << endl;
+	cout << "Part two result: " << solve_part_two(nums) << endl;
+
+	return 0;
+}
---
+++ Day2/day2.py
@@ -0,0 +1,10 @@
+#!/bin/python
+
+if __name__ == '__main__':
+	counter_var1 = 0
+	counter_var2 = 0
+	for arr in [[(int(line.strip('\n').split(' ')[0].split("-")[0]), int(line.strip('\n').split(' ')[0].split('-')[-1])), line.strip('\n').split(' ')[1].strip(':'), line.strip('\n').split(' ')[-1]] for line in open('input.txt', 'r').readlines()]:
+		counter_var1 += (arr[0][0] <= arr[2].count(arr[1]) <= arr[0][1])
+		counter_var2 += ((arr[2][arr[0][0] - 1] == arr[1]) ^ (arr[2][arr[0][1] - 1] == arr[1]))
+	print(f"Day2 Part1: {counter_var1}")
+	print(f"Day2 Part2: {counter_var2}")
