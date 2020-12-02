4b52580c87d69fd829a2a57364b4b8c2c4a49609
Tiago Matinho <tiagomartinho97@gmail.com>
2020-12-02 18:18:27 +0000

    Day 2 frame

---
+++ Day 2/solve.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solve_part_one() {
+  
+  return -1;
+}
+
+int solve_part_two() {
+  
+  return -1;
+}
+
+
+int main() {
+  vector<int> nums;
+  vector<int> res;
+
+  int temp;
+
+  while(cin >> temp)
+    nums.push_back(temp);
+  
+  cout << "Part one result: " << solve_part_one() << endl;
+  cout << "Part two result: " << solve_part_two() << endl;
+
+  return 0;
+}
